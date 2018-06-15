0a1952a7ebe0c990f27a3525c0c8eecc6f57a13b
muyang <imuyang3072@gmail.com>
2018-06-15 17:46:04 +0800

    add reverseList.c

---
+++ C_C++/reverseList.c
@@ -0,0 +1,23 @@
+struct ListNode{
+        int val;
+        struct ListNode *next;
+};
+
+struct ListNode* reverseList(struct ListNode* head){
+    struct ListNode* new_head = NULL;
+    struct ListNode* old_next = NULL;
+    if(head == NULL)
+        return NULL;
+    while(head != NULL)
+    {
+        old_next = head->next;
+        head->next = new_head;
+        new_head = head;
+        head = old_next;
+    }
+    return new_head;
+}
+
+struct ListNode* recurReverseList(struct ListNode* head,ListNode* & new_head){
+
+}
